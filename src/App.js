034b51fce558e26ddfd6450d5f01611d85f06ea8
Matt Olsen <olsen_matthew@yahoo.com>
2016-10-16 11:20:00 -1000

    data rendering inside component, iterating through an array

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SurflineData from './fetch-surfline.js'
+import TestComponent from './fetch-surfline.js'
 
 class App extends Component {
   render() {
@@ -11,7 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Checkummm</h2>
         </div>
-        <SurflineData />
+        <TestComponent />
       </div>
     );
   }
--- src/fetch-surfline.js
+++ src/fetch-surfline.js
@@ -1,11 +1,32 @@
-import React, { Component } from 'react'
-import Fetch from 'react-fetch'
+import React from 'react'
 import Highcharts from 'highcharts'
 import ReactHighcharts from 'react-highcharts'
 
-class TestComponent extends Component{
+const TestComponent = React.createClass({
+  getInitialState: function(){
+     return { keyz: [], valuez: [] };
+  },
+  reqListener: function(res) {
+    let keyz = []
+    let valuez = []
+      JSON.parse(res.target.responseText,function(k, v){
+        if(k ===  'surf' || 'Surf'){
+          keyz.push(k)
+          valuez.push(v)
+        }
+      })
+      this.setState({keyz: keyz, valuez: valuez})
+      console.log(this.state,'this.state mannnnn')
+  },
+  componentWillMount: function() {
+    var oReq = new XMLHttpRequest();
+      oReq.addEventListener("load", this.reqListener);
+      oReq.open("GET", "http://api.surfline.com/v1/forecasts/4750?resources=surf,analysis&days=25&getAllSpots=false&units=e&usenearshore=true&interpolate=true");
+      oReq.send();
+  },
   render(){
-    const surf_conditions = this.props.Sort
+    console.log(this.state.valuez[133],'this ahhhhhh zero')
+
     let config = {
       title: {
             text: 'Combination chart'
@@ -71,51 +92,58 @@ class TestComponent extends Component{
     return (
       <div className="daHighchart">
         <ReactHighcharts config={config} />
+        <div className="reportCardContainer">
+          <div className="reportCardContent">
+            <img role="presentation" src="http://camstills.cdn-surfline.com/pipelinecam/latest_full.jpg" />
+            <h1>{this.state.valuez[4626]}</h1>
+            <div>{this.state.valuez[133]}</div>
+          </div>
+        </div>
       </div>
     )
   }
-}
+})
 
 
-class SurflineData extends Component {
-  render() {
-  let forecastDates = []
-  let waveHeights = []
-  let greatSuccess = (data) => {
-    console.log(data.Sort.dateStamp,'<----data.Sort.dateStamp');
-      data.Sort.dateStamp.map((el,index)=>{
-        forecastDates.push(el[0])
-        return forecastDates
-      })
-      data.Sort.height1.map((el,index)=>{
-        waveHeights.push(el[0])
-        return waveHeights
-      })
-    this.setState({
-      _forecastDates: forecastDates,
-      _waveHeights: waveHeights
-    })
-  }
-  console.log(forecastDates,'fd')
-  console.log(waveHeights,'waveHeights')
-    return (
-      <div>
-        <img role="presentation" src="http://camstills.cdn-surfline.com/pipelinecam/latest_full.jpg" />
-        <Fetch onSuccess={greatSuccess} url="http://api.surfline.com/v1/forecasts/4750?resources=surf,analysis,wind,weather,tide,sort&days=25&getAllSpots=false&units=e&usenearshore=true&interpolate=true">
-          <TestComponent />
-        </Fetch>
-      </div>
-    )
-  }
-}
+// class SurflineData extends Component {
+//   render() {
+//   let forecastDates = []
+//   let waveHeights = []
+//   let greatSuccess = (data) => {
+//     console.log(data.Sort.dateStamp,'<----data.Sort.dateStamp');
+//       data.Sort.dateStamp.map((el,index)=>{
+//         forecastDates.push(el[0])
+//         return forecastDates
+//       })
+//       data.Sort.height1.map((el,index)=>{
+//         waveHeights.push(el[0])
+//         return waveHeights
+//       })
+//     this.setState({
+//       _forecastDates: forecastDates,
+//       _waveHeights: waveHeights
+//     })
+//   }
+//   console.log(forecastDates,'fd')
+//   console.log(waveHeights,'waveHeights')
+//     return (
+//       <div>
+//         <img role="presentation" src="http://camstills.cdn-surfline.com/pipelinecam/latest_full.jpg" />
+//         <Fetch onSuccess={greatSuccess} url="http://api.surfline.com/v1/forecasts/4750?resources=surf,analysis,wind,weather,tide,sort&days=25&getAllSpots=false&units=e&usenearshore=true&interpolate=true">
+//           <TestComponent dateStamp={forecastDates} />
+//         </Fetch>
+//       </div>
+//     )
+//   }
+// }
 
-TestComponent.propTypes = {
-  surf_conditions: React.PropTypes.objectOf(React.PropTypes.array)
-}
+// TestComponent.propTypes = {
+//   surf_conditions: React.PropTypes.objectOf(React.PropTypes.array)
+// }
 
-SurflineData.propTypes = {
-  banana: React.PropTypes.array
-}
+// SurflineData.propTypes = {
+//   banana: React.PropTypes.array
+// }
 
 
-export default SurflineData
+export default TestComponent
